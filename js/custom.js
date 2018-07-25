adce8ab1bf8885962b0e372d4181da6f61b1aa32
cahupa99 <cahupa99@gmail.com>
2018-07-24 19:37:06 -0500

    contacto listo y prueba correo

--- js/custom.js
+++ js/custom.js
@@ -25,17 +25,6 @@
 	    count($(this));
 	});
 
-
-
-	$('.header').affix({
-	    offset: {
-	        top: 100,
-	        bottom: function() {
-	            return (this.bottom = $('.footer').outerHeight(true))
-	        }
-	    }
-	})
-
 	$(window).load(function() {
 	    $("#preloader").on(500).fadeOut();
 	    $(".preloader").on(600).fadeOut("slow");
